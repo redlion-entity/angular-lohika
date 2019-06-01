c41d554c7dc287edb582d8026d92f96b4c69f0ce
redlion-entity <redlion.2016@ya.ru>
2019-06-01 22:33:47 +0300

    Made as a blank App

---
+++ lecture-1/src/components/App/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import style from './style.module.scss';
+
+
+const App: React.FC = () => (
+    <div className={style.container}>
+        <div className={style.innerContainer}>
+            <div className={style.content}>
+
+            </div>
+        </div>
+    </div>
+);
+
+export default App;
